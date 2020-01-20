3c826c7063e1c565629e84ed3606bf09dbdb518c
pawelroman7 <pawelroman7@gmail.com>
2020-01-20 15:38:01 +0100

    initial

---
+++ src/main/java/pawelroman7/ftech_workshop/FtechWorkshopApplication.java
@@ -0,0 +1,13 @@
+package pawelroman7.ftech_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtechWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtechWorkshopApplication.class, args);
+    }
+
+}
