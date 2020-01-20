8d63e9a1af30ba2962d1fa64658a4d56f401ce6e
pawelroman7 <pawelroman7@gmail.com>
2020-01-20 15:44:43 +0100

    initial

---
+++ src/test/java/pawelroman7/ftech_workshop/FtechWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pawelroman7.ftech_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtechWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
